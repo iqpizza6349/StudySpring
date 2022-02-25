028749f1c2752aa752bdaf936f9740e50ef8ce7f
iqpizza6349 <iqpizza6349@gmail.com>
2022-02-26 03:19:06 +0900

    토큰 재발급 기능 추가

--- src/main/java/com/tistory/workshop/studyspring/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/tistory/workshop/studyspring/domain/repository/RefreshTokenRepository.java
@@ -2,7 +2,6 @@ package com.tistory.workshop.studyspring.domain.repository;
 
 import com.tistory.workshop.studyspring.domain.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
